d7763ed5dafae5b1e7897508e3216785003b2d3b
code-cloud9 <wenqindu@marshall.usc.edu>
2026-06-10 16:48:03 -0700

    add plot function

--- simulation_appendix/typeIerror-power.R
+++ simulation_appendix/typeIerror-power.R
@@ -1,16 +1,92 @@
 library(readr)
-test_stat <- read_csv("b-c0.2-normal-Net1.8.csv")
-# b-c0-normal-Net1
-# b-c0-normal-Net1.2
-# b-c0-normal-Net1.4
-# b-c0-normal-Net1.6
-# b-c0-normal-Net1.8
+test_stat <- read_csv("a-c0.2-normal-Net1.8.csv")
+# a-c0-normal-Net1
+# a-c0-normal-Net1.2
+# a-c0-normal-Net1.4
+# a-c0-normal-Net1.6
+# a-c0-normal-Net1.8
 
-# b-c0.2-normal-Net1
-# b-c0.2-normal-Net1.2
-# b-c0.2-normal-Net1.4
-# b-c0.2-normal-Net1.6
-# b-c0.2-normal-Net1.8
+# a-c0.2-normal-Net1
+# a-c0.2-normal-Net1.2
+# a-c0.2-normal-Net1.4
+# a-c0.2-normal-Net1.6
+# a-c0.2-normal-Net1.8
 
 apply(abs(test_stat) > qnorm(0.975,0,1), 2, sum) / 1000 
+# Null
+# 0.048 0.049 0.051 0.065
+# 0.051 0.048 0.062 0.049
+# 0.061 0.047 0.053 0.051
+# 0.050 0.056 0.043 0.052 
+# 0.065 0.066 0.050 0.039 
+
+# Alt
+# 0.774 0.950 1.000 1.000 
+# 0.950 0.999 1.000 1.000
+# 0.999 1.000 1.000 1.000 
+# 1  1  1  1
+# 1  1  1  1 
+
+color_list <- c(# rgb(210/252,237/252,252/252), 
+                rgb(135/252,206/252,250/252), 
+                rgb(22/252,120/252,245/252),
+                rgb(2/252,61/252,128/252))
+
+par(mfrow=c(1,2),omi=c(0,0,0,0.02))  # , plt=c(0.18,1,0.23,0.85)
+
+
+matrix_null <- matrix(c(0.048, 0.049, 0.051,
+                        0.051, 0.048, 0.062,
+                        0.061, 0.047, 0.053,
+                        0.050, 0.056, 0.043,
+                        0.065, 0.066, 0.050),3,5)
+
+matrix_alt <- matrix(c(0.774, 0.950, 1.000,
+                       0.950, 0.999, 1.000,
+                       0.999, 1.000, 1.000,
+                       1, 1, 1, 1, 1, 1),3,5)
+
+matplot(t(matrix_null),
+        xaxt="n",
+        type = "b",
+        pch = 21:23,
+        cex = 0.6,
+        lwd = 2,
+        las = 2,
+        lty = 1:3,
+        col = color_list,
+        cex.lab=1.3,
+        cex.axis=1.1,
+        # col=c("pink", "red", "orange", "blue"),
+        # main="(a) Poisson",  # Poisson t-distribution
+        ylab="Type-I error rate",
+        xlab=expression(lambda),
+        ylim = c(0,0.1))
+# title("(a) Poisson", adj = 0.5, line = 0.6)
+abline(h=0.05, col="red", lty = 4, lwd = 1.6)
+axis(1,at =c(1,2,3,4,5),label=c(1,1.2,1.4,1.6,1.8),cex.axis=1.1,las=1)
+legend(3.63, 0.034, legend=c("n=50", "n=100", "n=200"), pch = 21:23,
+       lty = 1:3, lwd = 1.2, cex=0.96, col=color_list)  
+
+
+matplot(t(matrix_alt),
+        xaxt="n",
+        type = "b",
+        pch = 21:23,
+        cex = 0.6,
+        lwd = 2,
+        las = 2,
+        lty = 1:3,
+        col = color_list,
+        cex.lab=1.3,
+        cex.axis=1.1,
+        # col=c("pink", "red", "orange", "blue"),
+        # main="(a) Poisson",  # Poisson t-distribution
+        ylab="power",
+        xlab=expression(lambda),
+        ylim = c(0.75,1))
+# title("(a) Poisson", adj = 0.5, line = 0.6)
+axis(1,at =c(1,2,3,4,5),label=c(1,1.2,1.4,1.6,1.8),cex.axis=1.1,las=1)
+legend(3.63, 0.835, legend=c("n=50", "n=100", "n=200"), pch = 21:23,
+       lty = 1:3, lwd = 1.2, cex=0.96, col=color_list)  
 
